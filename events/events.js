5ea2eae1e2faf04ce1f191b19b55812f3db17564
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:27:47 +0300

    very nice random button

--- events/events.js
+++ events/events.js
@@ -9,4 +9,10 @@ function randomY() {
 }
 /* END PROVIDED CODE */
 
-// Write your code below this line :)
+
+$("#moveButton").on('click',function(){
+	$("#message").css('top', randomY()+'px').css('left', randomX()+'px');
+
+
+});
+
--- style/style.js
+++ style/style.js
@@ -6,10 +6,12 @@ var z = 0; // rotation of Z
 
 function updateSize(newSize) {
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
+  $('img').eq(0).css("width",newSize);
 }
 
 function updateRotation() {
   // FINISH ME! Set the rotation of the MEET logo based on the values of (x, y, z) (see above!)
+  $('img').css("transform", "rotateX("+x.toString()+"deg) rotateY("+y.toString()+"deg) rotateZ("+z.toString()+"deg)");
 }
 
 function updateXRotation(newX) {
